d0160a3594eabb445643df6be74c6738b9af4fb2
alchemistbg <alchemist_bg@abv.bg>
2023-01-17 19:54:46 +0100

    Add solution to the fifth problem from exam that was held on 05.2019

---
+++ z-Python-01-Programming-Basics-Python/Exams/2019.05/05_Division_Without_Remainder.py
@@ -0,0 +1,25 @@
+# 100/100
+
+counter = int(input())
+
+p1 = 0
+p2 = 0
+p3 = 0
+
+for _ in range(counter):
+    number = int(input())
+
+    if number % 2 == 0:
+        p1 += 1
+    if number % 3 == 0:
+        p2 += 1
+    if number % 4 == 0:
+        p3 += 1
+
+p1_percentage = p1 / counter * 100
+p2_percentage = p2 / counter * 100
+p3_percentage = p3 / counter * 100
+
+print(f'{p1_percentage:.2f}%')
+print(f'{p2_percentage:.2f}%')
+print(f'{p3_percentage:.2f}%')
