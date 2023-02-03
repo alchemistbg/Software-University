8d090e9a69472613797232a59e204760af535f32
alchemistbg <alchemist_bg@abv.bg>
2023-02-03 20:55:14 +0100

    Add the solution to a problem, that is a part of old version of the exercise for the lecture 'Lists - Advanced'

---
+++ z-Python-02-Fundamentals/17_-_E_-_Lists_-_Advanced/z_01_Big_Number_Lovers.py
@@ -0,0 +1,8 @@
+# This problem has been removed form the current course instance, but is on the one that I have
+
+# strings = input().split()
+strings = '3 30 34 5 9'.split()
+strings.sort()
+strings.reverse()
+print("".join(strings))
+
