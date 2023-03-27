b8b182bd63c2d3827b567580dc9e383771901310
alchemistbg <alchemist_bg@abv.bg>
2023-03-27 19:43:53 +0200

    Add solution to the 2st additional problem from the lab for the lecture 'Functions Advanced'

---
+++ z-Python-03-Advanced/11_-_L_-_Functions_Advanced/z_-_02_Rounding.py
@@ -0,0 +1,4 @@
+# 100/100
+# This problem has been solved in Python Fundamentals course. Here a little bite different approach was used
+
+print(list(map(lambda x: round(float(x)), input().split())))
