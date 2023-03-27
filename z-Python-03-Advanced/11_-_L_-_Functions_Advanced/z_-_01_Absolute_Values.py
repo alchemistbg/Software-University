ba1279da32bef1fd4cd6e26d8dbb86fcb729abed
alchemistbg <alchemist_bg@abv.bg>
2023-03-27 19:43:26 +0200

    Add solution to the 1st additional problem from the lab for the lecture 'Functions Advanced'

---
+++ z-Python-03-Advanced/11_-_L_-_Functions_Advanced/z_-_01_Absolute_Values.py
@@ -0,0 +1,5 @@
+# 100/100
+# This problem has been solved in Python Fundamentals course. Here is a different approach was used
+
+numbers = list(map(float, input().split()))
+print(list(map(abs, numbers)))
