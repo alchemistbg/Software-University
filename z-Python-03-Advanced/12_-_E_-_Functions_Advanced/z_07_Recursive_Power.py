e34b7a916e1486c4663bde7e2bf8654baaea1267
alchemistbg <alchemist_bg@abv.bg>
2023-04-03 19:51:27 +0200

    Add solution to the 7th additional problem from the exercise for the lecture 'Functions Advanced'

---
+++ z-Python-03-Advanced/12_-_E_-_Functions_Advanced/z_07_Recursive_Power.py
@@ -0,0 +1,12 @@
+# This problem is solved in the lab
+
+def recursive_power(number, power):
+    if power == 1:
+        return number
+
+    return number * recursive_power(number, power - 1)
+
+
+print(recursive_power(2, 10))
+print(72 * '*')
+print(recursive_power(10, 100))
