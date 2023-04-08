ad03e48d49b592bcd0639026315e85ca01ed8ce8
alchemistbg <alchemist_bg@abv.bg>
2023-04-08 22:31:57 +0200

    Add solution to the 3nd problem from the lab for the lecture 'Modules'

---
+++ z-Python-03-Advanced/15_-_L_-_Modules/03_Triangle.py
@@ -0,0 +1,7 @@
+# I cannot test this solution, because the problem doesn't exist in the judge system
+# My solution is cleaner. It does not produce empty row
+
+import triangle_module
+
+n = int(input())
+triangle_module.print_triangle(n + 1)
