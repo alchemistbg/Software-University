20a3b43df897c9942435f3f89e52a25d70cbe572
alchemistbg <alchemist_bg@abv.bg>
2023-04-16 20:39:26 +0200

    Add solution to the 5th problem from the lab for the lecture 'Defining Classes'

---
+++ z-Python-03-OOP/03_-_L_-_Defining_Classes/05_Music.py
@@ -0,0 +1,32 @@
+class Music:
+    def __init__(self, title: str, artist: str, lyrics: str ):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self):
+        return f'This is "{self.title}" from "{self.artist}"'
+
+    def play(self):
+        return self.lyrics
+
+
+# from dataclasses import dataclass
+#
+#
+# @dataclass
+# class Music:
+#     title: str
+#     artist: str
+#     lyrics: str
+#
+#     def print_info(self):
+#         return f'This is "{self.title}" from "{self.artist}"'
+#
+#     def play(self):
+#         return self.lyrics
+
+
+song = Music("Title", "Artist", "Lyrics")
+print(song.print_info())
+print(song.play())
