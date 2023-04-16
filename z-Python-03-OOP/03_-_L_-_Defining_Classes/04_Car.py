ea3a7789fc0ec4af3aa0ef895ac6d7de6f06e839
alchemistbg <alchemist_bg@abv.bg>
2023-04-16 20:39:07 +0200

    Add solution to the 4th problem from the lab for the lecture 'Defining Classes'

---
+++ z-Python-03-OOP/03_-_L_-_Defining_Classes/04_Car.py
@@ -0,0 +1,27 @@
+# 100/100
+
+class Car:
+    def __init__(self, name: str, model: str, engine: str):
+        self.name = name
+        self.model = model
+        self.engine = engine
+
+    def get_info(self):
+        return f"This is {self.name} {self.model} with engine {self.engine}"
+
+
+# from dataclasses import dataclass
+#
+#
+# @dataclass
+# class Car:
+#     name: str
+#     model: str
+#     engine: str
+#
+#     def get_info(self):
+#         return f"This is {self.name} {self.model} with engine {self.engine}"
+
+
+car = Car("Kia", "Rio", "1.3L B3 I4")
+print(car.get_info())
