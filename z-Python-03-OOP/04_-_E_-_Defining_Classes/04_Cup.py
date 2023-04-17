d8bdc75961d5c5f090bb513b41b12fb12e4d053c
alchemistbg <alchemist_bg@abv.bg>
2023-04-17 20:29:50 +0200

    Add solution to the 4th problem from the exercise for the lecture 'Defining Classes'

---
+++ z-Python-03-OOP/04_-_E_-_Defining_Classes/04_Cup.py
@@ -0,0 +1,21 @@
+# 100/100
+
+class Cup:
+    def __init__(self, size: int, quantity: int):
+        self.size = size
+        self.quantity = quantity
+
+    def fill(self, quantity: int):
+        new_quantity = self.quantity + quantity
+        if new_quantity <= self.size:
+            self.quantity = new_quantity
+
+    def status(self):
+        return self.size - self.quantity
+
+
+cup = Cup(100, 50)
+print(cup.status())
+cup.fill(40)
+cup.fill(20)
+print(cup.status())
