7bcff0b902e84e6eeab26e6d5c827fe7ca8bf17c
alchemistbg <alchemist_bg@abv.bg>
2023-04-26 20:27:35 +0200

    Add solution to the 3rd problem from the exercise for the lecture 'Classes and Instances'

---
+++ z-Python-03-OOP/06_-_E_-_Classes_and_Instances/03_Account.py
@@ -0,0 +1,35 @@
+# 100/100
+
+
+class Account:
+
+    def __init__(self, account_id: str, name: str, balance: int = 0):
+        self.id = account_id
+        self.name = name
+        self.balance = balance
+
+    def credit(self, amount) -> int:
+        self.balance += amount
+        return self.balance
+
+    def debit(self, amount):
+        if amount > self.balance:
+            return "Amount exceeded balance"
+
+        self.balance -= amount
+        return self.balance
+
+    def info(self):
+        return f"User {self.name} with account {self.id} has {self.balance} balance"
+
+
+account_1 = Account(1234, "George", 1000)
+print(account_1.credit(500))
+print(account_1.debit(1500))
+print(account_1.info())
+
+account_2 = Account(5411256, "Peter")
+print(account_2.debit(500))
+print(account_2.credit(1000))
+print(account_2.debit(500))
+print(account_2.info())
