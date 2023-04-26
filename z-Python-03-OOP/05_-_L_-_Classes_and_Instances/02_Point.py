9a8cb362da88c4e3ba2655505102313c78ba0f74
alchemistbg <alchemist_bg@abv.bg>
2023-04-26 20:26:27 +0200

    Add 'distance' method
    This method calculates euclidean distance between two points

--- z-Python-03-OOP/05_-_L_-_Classes_and_Instances/02_Point.py
+++ z-Python-03-OOP/05_-_L_-_Classes_and_Instances/02_Point.py
@@ -1,18 +1,26 @@
 # 100/100
+import math
+
 
 class Point:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
-    def set_x(self, new_x):
+    def set_x(self, new_x: int):
         self.x = new_x
 
-    def set_y(self, new_y):
+    def set_y(self, new_y: int):
         self.y = new_y
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"The point has coordinates ({self.x},{self.y})"
+
+    # This method is added based on the old version of the task
+    def distance(self, x, y) -> float:
+        diff_x = (self.x - x)**2
+        diff_y = (self.y - y)**2
+        return math.sqrt(diff_x + diff_y)
 
 
 p = Point(2, 4)
@@ -20,3 +28,4 @@ print(p)
 p.set_x(3)
 p.set_y(5)
 print(p)
+print(p.distance(10, 2))
