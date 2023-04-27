ee4ec75643aad42827cbf5dab4caf3e7c7540bd3
alchemistbg <alchemist_bg@abv.bg>
2023-04-27 19:28:10 +0200

    Perform minor code refactoring
    Add return type to all of the methods
    
    Change name of 'id' variable to 'employee_id'

--- z-Python-03-OOP/04_-_E_-_Defining_Classes/03_Employee.py
+++ z-Python-03-OOP/04_-_E_-_Defining_Classes/03_Employee.py
@@ -1,19 +1,19 @@
 # 100/100
 
 class Employee:
-    def __init__(self, id: int, first_name: str, last_name: str, salary: int):
-        self.id = id
+    def __init__(self, employee_id: int, first_name: str, last_name: str, salary: int):
+        self.id = employee_id
         self.first_name = first_name
         self.last_name = last_name
         self.salary = salary
 
-    def get_full_name(self):
+    def get_full_name(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
-    def get_annual_salary(self):
+    def get_annual_salary(self) -> int:
         return self.salary * 12
 
-    def raise_salary(self, amount: int):
+    def raise_salary(self, amount: int) -> int:
         self.salary += amount
         return self.salary
 
