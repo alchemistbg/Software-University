ee64787f6b671fc3de1f806b7c61138227a31fd7
alchemistbg <alchemist_bg@abv.bg>
2023-04-28 21:08:31 +0200

    Fix type hits in the code

--- z-Python-03-OOP/06_-_E_-_Classes_and_Instances/03_Account.py
+++ z-Python-03-OOP/06_-_E_-_Classes_and_Instances/03_Account.py
@@ -1,5 +1,6 @@
 # 100/100
-
+from typing import Union
+# This is imported in order to return different types of variables from one method
 
 class Account:
 
@@ -8,11 +9,11 @@ class Account:
         self.name = name
         self.balance = balance
 
-    def credit(self, amount) -> int:
+    def credit(self, amount: int) -> int:
         self.balance += amount
         return self.balance
 
-    def debit(self, amount):
+    def debit(self, amount: int) -> Union[int, str]:
         if amount > self.balance:
             return "Amount exceeded balance"
 
@@ -27,6 +28,8 @@ account_1 = Account(1234, "George", 1000)
 print(account_1.credit(500))
 print(account_1.debit(1500))
 print(account_1.info())
+
+print(72*'-')
 
 account_2 = Account(5411256, "Peter")
 print(account_2.debit(500))
