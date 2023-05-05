d4fe0d612190ce6cec1d642f55e970a38ab2d52a
alchemistbg <alchemist_bg@abv.bg>
2023-05-05 19:27:52 +0200

    Rename class in order to match with the task description
    Add @classmethod for generating store objects

--- z-Python-03-OOP/07_-_L_-_Static_and_Class_Methods/02_Store_01.py
+++ z-Python-03-OOP/07_-_L_-_Static_and_Class_Methods/02_Store_01.py
@@ -4,15 +4,20 @@ from typing import Dict
 
 
 class Store:
-    def __init__(self, name: str, type: str, capacity: float):
+    def __init__(self, name: str, type: str, capacity: int):
         self.name = name
         self.type = type
         self.capacity = capacity
         self.items: Dict[str, int] = {}
 
-    @staticmethod
-    def from_size(name: str, type: str, size: int):
-        return Store(name, type, size)
+    # @staticmethod
+    # def from_size(name: str, type: str, size: int):
+    #     return Store(name, type, size)
+
+    @classmethod
+    def from_size(cls, name: str, type: str, size: int):
+        corrected_size = size // 2
+        return cls(name, type, corrected_size)
 
     def add_item(self, item_name: str):
         if item_name not in self.items.keys():
@@ -38,14 +43,14 @@ class Store:
         return f"{self.name} of type {self.type} with capacity {self.capacity}"
 
 
-first_store = Store("First store", "Fruit and Veg", 1)
+first_store = Store("First store", "Fruit and Veg", 20)
 second_store = Store.from_size("Second store", "Clothes", 500)
 
 print(first_store)
 print(second_store)
 
 print(first_store.add_item("potato"))
-print(first_store.add_item("potato"))
 print(second_store.add_item("jeans"))
 print(first_store.remove_item("tomatoes", 1))
 print(second_store.remove_item("jeans", 1))
+
