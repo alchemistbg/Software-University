96ba686d01d8679ec259afd03ba2ceabe3ad65b5
alchemistbg <alchemist_bg@abv.bg>
2023-05-07 20:55:31 +0200

    Implement solution using oprator module instead of lambda functions

--- z-Python-03-OOP/07_-_L_-_Static_and_Class_Methods/01_Calculator.py
+++ z-Python-03-OOP/07_-_L_-_Static_and_Class_Methods/01_Calculator.py
@@ -1,28 +1,32 @@
 # 100/100
 
 from functools import reduce
-
+import operator
 
 class Calculator:
 
     @staticmethod
     def add(*args):
-        result = sum(args)
+        # result = reduce(lambda a, b: a + b, args)
+        result = reduce(operator.add, args)
         return result
 
     @staticmethod
     def multiply(*args):
-        result = reduce(lambda a, b: a * b, args)
+        # result = reduce(lambda a, b: a * b, args)
+        result = reduce(operator.mul, args)
         return result
 
     @staticmethod
     def divide(*args):
-        result = reduce(lambda a, b: a / b, args)
+        # result = reduce(lambda a, b: a / b, args)
+        result = reduce(operator.truediv, args)
         return result
 
     @staticmethod
     def subtract(*args):
-        result = reduce(lambda a, b: a - b, args)
+        # result = reduce(lambda a, b: a - b, args)
+        result = reduce(operator.sub, args)
         return result
 
 
