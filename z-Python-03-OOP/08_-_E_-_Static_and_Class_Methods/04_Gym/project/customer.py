9829acdf5dcb70da23296e47131da728f8503d64
alchemistbg <alchemist_bg@abv.bg>
2023-05-14 12:52:56 +0200

    Apply some minor code refactoring

--- z-Python-03-OOP/08_-_E_-_Static_and_Class_Methods/04_Gym/project/customer.py
+++ z-Python-03-OOP/08_-_E_-_Static_and_Class_Methods/04_Gym/project/customer.py
@@ -1,19 +1,29 @@
-from itertools import count
+# from itertools import count
 
 
 class Customer:
+    # id = count(start = 1)
     # The lecturer uses different approach for class id for this and for other classes
-    id = count(start = 1)
+    # This could be used in all classes
+    _id = 1
 
     def __init__(self, name: str, address: str, email: str):
         self.name = name
         self.address = address
         self.email = email
-        self.id = Customer.get_next_id()
+        # self.id = Customer.get_next_id()
+        # Part of lecturer's approach
+        self.id = self._id
+        self.__class__._id += 1
 
-    @staticmethod
-    def get_next_id():
-        return next(Customer.id)
+    # @staticmethod
+    # def get_next_id():
+    #     return next(Customer.id)
+
+    # Part of lecturer's approach
+    @classmethod
+    def get_next_id(cls):
+        return cls._id
 
     def __repr__(self):
         return f"Customer <{self.id}> {self.name}; Address: {self.address}; Email: {self.email}"
--- z-Python-03-OOP/08_-_E_-_Static_and_Class_Methods/04_Gym/project/gym.py
+++ z-Python-03-OOP/08_-_E_-_Static_and_Class_Methods/04_Gym/project/gym.py
@@ -1,3 +1,4 @@
+from typing import List
 from .customer import Customer
 from .equipment import Equipment
 from .exercise_plan import ExercisePlan
@@ -8,51 +9,78 @@ from .trainer import Trainer
 class Gym:
 
     def __init__(self):
-        self.customers = []
-        self.trainers = []
-        self.equipment = []
-        self.plans = []
-        self.subscriptions = []
+        self.customers: List[Customer] = []
+        self.trainers: List[Trainer] = []
+        self.equipment: [Equipment] = []
+        self.plans: List[ExercisePlan] = []
+        self.subscriptions: List[Subscription] = []
 
-    def add_customer(self, customer: Customer):
-        customers_ids = [customer.id for customer in self.customers]
-        if customer.id not in  customers_ids:
+    def add_customer(self, customer: Customer) -> None:
+        # customers_ids = [customer.id for customer in self.customers]
+        # if customer.id not in customers_ids:
+        #     self.customers.append(customer)
+        if customer not in self.customers:
             self.customers.append(customer)
 
-    def add_trainer(self, trainer: Trainer):
-        trainers_ids = [trainer.id for trainer in self.trainers]
-        if trainer.id not in trainers_ids:
+    def add_trainer(self, trainer: Trainer) -> None:
+        # trainers_ids = [trainer.id for trainer in self.trainers]
+        # if trainer.id not in trainers_ids:
+        #     self.trainers.append(trainer)
+        if trainer not in self.trainers:
             self.trainers.append(trainer)
 
-    def add_equipment(self, equipment: Equipment):
-        equipments_ids = [equipment.id for equipment in self.equipment]
-        if equipment.id not in equipments_ids:
+    def add_equipment(self, equipment: Equipment) -> None:
+        # equipments_ids = [equipment.id for equipment in self.equipment]
+        # if equipment.id not in equipments_ids:
+        #     self.equipment.append(equipment)
+        if equipment not in self.equipment:
             self.equipment.append(equipment)
 
-    def add_plan(self, plan: ExercisePlan):
-        plans_ids = [plan.id for plan in self.plans]
-        if plan.id not in plans_ids:
+    def add_plan(self, plan: ExercisePlan) -> None:
+        # plans_ids = [plan.id for plan in self.plans]
+        # if plan.id not in plans_ids:
+        #     self.plans.append(plan)
+        if plan not in self.plans:
             self.plans.append(plan)
 
-    def add_subscription(self, subscription: Subscription):
-        subscriptions_ids = [subscription.id for subscription in self.subscriptions]
-        if subscription.id not in subscriptions_ids:
+    def add_subscription(self, subscription: Subscription) -> None:
+        # subscriptions_ids = [subscription.id for subscription in self.subscriptions]
+        # if subscription.id not in subscriptions_ids:
+        #     self.subscriptions.append(subscription)
+        if subscription not in self.subscriptions:
             self.subscriptions.append(subscription)
 
     def subscription_info(self, subscription_id: int):
         subscriptions_ids = [subscription.id for subscription in self.subscriptions]
         info = []
+
+        # if subscription_id in subscriptions_ids:
+        #     idx = subscriptions_ids.index(subscription_id)
+        #     subscription = self.subscriptions[idx]
+        #     info.append(str(subscription))
+        #     customer = self.customers[idx]
+        #     info.append(str(customer))
+        #     trainer = self.trainers[idx]
+        #     info.append(str(trainer))
+        #     equipment = self.equipment[idx]
+        #     info.append(str(equipment))
+        #     plan = self.plans[idx]
+        #     info.append(str(plan))
+
         if subscription_id in subscriptions_ids:
-            idx = subscriptions_ids.index(subscription_id)
-            subscription = self.subscriptions[idx]
+            subscription = self.subscriptions[subscription_id - 1]
             info.append(str(subscription))
-            customer = self.customers[idx]
+
+            customer = self.customers[subscription.customer_id - 1]
             info.append(str(customer))
-            trainer = self.trainers[idx]
+
+            trainer = self.trainers[subscription.trainer_id - 1]
             info.append(str(trainer))
-            equipment = self.equipment[idx]
+
+            plan = self.plans[subscription.trainer_id - 1]
+            equipment = self.equipment[plan.equipment_id - 1]
+
             info.append(str(equipment))
-            plan = self.plans[idx]
             info.append(str(plan))
 
         return "\n".join(info)
