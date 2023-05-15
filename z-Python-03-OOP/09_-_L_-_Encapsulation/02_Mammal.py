d51765315996ad90a5a898592797db37a38f96a6
alchemistbg <alchemist_bg@abv.bg>
2023-05-15 20:05:31 +0200

    Implement solution for the second problem

--- z-Python-03-OOP/09_-_L_-_Encapsulation/02_Mammal.py
+++ z-Python-03-OOP/09_-_L_-_Encapsulation/02_Mammal.py
@@ -1,2 +1,33 @@
+# 100/100
+
 class Mammal:
-    pass
+    __kingdom = "animals"
+
+    def __init__(self, name: str, type: str, sound: str):
+        self.name = name
+        self.type = type
+        self.sound = sound
+
+    def make_sound(self) -> str:
+        return f"{self.name} makes {self.sound}"
+
+    # This must be instance method (according problem descriptions)
+    def get_kingdom(self) -> str:
+        # One way
+        return self.__kingdom
+        # Another way
+        # return Mammal.__kingdom
+
+    # This is a third way for achieving the upper functionality
+    # @classmethod
+    # def get_kingdom(cls) -> str:
+    #     return cls.__kingdom
+
+    def info(self):
+        return f'{self.name} is of type {self.type}'
+
+
+mammal = Mammal("Dog", "Domestic", "Bark")
+print(mammal.make_sound())
+print(mammal.get_kingdom())
+print(mammal.info())
