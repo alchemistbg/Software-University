7172089c70158188e607bba0475cdc48c4447dca
alchemistbg <alchemist_bg@abv.bg>
2023-05-16 19:55:35 +0200

    Implement solution to the 4th problem from the lab for the lecture 'Encapsulation'

--- z-Python-03-OOP/09_-_L_-_Encapsulation/04_Email_Validator.py
+++ z-Python-03-OOP/09_-_L_-_Encapsulation/04_Email_Validator.py
@@ -1,2 +1,35 @@
+# 100/100
+from typing import List
+import re
+
+
 class EmailValidator:
-    pass
+
+    def __init__(self, min_length: int, mails: List[str], domains: List[str]):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, username) -> bool:
+        return len(username) >= self.min_length
+
+    def __is_mail_valid(self, mail) -> bool:
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain) -> bool:
+        return domain in self.domains
+
+    def validate(self, email) -> bool:
+        pattern = r"(\w*)@(\w*).(\w*)"
+        username, mail, domain = re.findall(pattern, email)[0]
+        return self.__is_name_valid(username) and self.__is_mail_valid(mail) and\
+            self.__is_domain_valid(domain)
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
