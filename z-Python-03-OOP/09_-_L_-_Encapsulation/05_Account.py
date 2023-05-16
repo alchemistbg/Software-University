8859466072997392198a5465d411ececf410725d
alchemistbg <alchemist_bg@abv.bg>
2023-05-16 19:55:47 +0200

    Implement solution to the 5th problem from the lab for the lecture 'Encapsulation'

--- z-Python-03-OOP/09_-_L_-_Encapsulation/05_Account.py
+++ z-Python-03-OOP/09_-_L_-_Encapsulation/05_Account.py
@@ -1,2 +1,27 @@
+# 100/100
+from typing import Union
+
+
 class Account:
-    pass
+
+    def __init__(self, id: int, balance: float, pin: int):
+        self.__id = id
+        self.__pin = pin
+        self.balance = balance
+
+    def __is_pin_correct(self, pin: int) -> int:
+        return self.__pin == pin
+
+    def get_id(self, pin: int) -> Union[int, str]:
+        if self.__is_pin_correct(pin):
+            return self.__id
+        return 'Wrong pin'
+
+    def change_pin(self, old_pin: int, new_pin: int) -> str:
+        if self.__is_pin_correct(old_pin):
+            self.__pin = new_pin
+            return "Pin changed"
+
+        return "Wrong pin"
+
+
