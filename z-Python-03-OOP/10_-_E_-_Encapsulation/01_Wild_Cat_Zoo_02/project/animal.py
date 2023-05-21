3db46bf8da5565a16f07225d470a3212dad65b63
alchemistbg <alchemist_bg@abv.bg>
2023-05-21 14:52:06 +0200

    Remove 'money_for_care' property
    It is transfered to separate animal types

--- z-Python-03-OOP/10_-_E_-_Encapsulation/01_Wild_Cat_Zoo_02/project/animal.py
+++ z-Python-03-OOP/10_-_E_-_Encapsulation/01_Wild_Cat_Zoo_02/project/animal.py
@@ -1,21 +1,9 @@
 class Animal:
+
     def __init__(self, name: str, gender: str, age: int, money_for_care: int = 0):
         self.name = name
         self.gender = gender
         self.age = age
-
-    @property
-    def money_for_care(self):
-        class_type = str(self.__class__)
-        if 'Lion' in class_type:
-            return 50
-        elif 'Tiger' in class_type:
-            return 45
-        elif 'Cheetah' in class_type:
-            return 60
-        else:
-            return 0
-
 
     def __repr__(self):
         return f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}"
