3af6bbda62bfa7f41af6c1f74c4082e223a0d00f
alchemistbg <alchemist_bg@abv.bg>
2023-05-30 18:49:07 +0200

    Adding package exports in the __init__.py file

---
+++ z-Python-03-OOP/12_-_E_-_Inheritance/02_Zoo/project/__init__.py
@@ -0,0 +1,17 @@
+from project.animal import Animal
+from project.mammal import Mammal
+from project.bear import Bear
+from project.gorilla import Gorilla
+from project.reptile import Reptile
+from project.lizard import Lizard
+from project.snake import Snake
+
+__all__ = [
+	'Animal',
+	'Mammal',
+	'Bear',
+	'Gorilla',
+	'Reptile',
+	'Lizard',
+	'Snake',
+]
