11580d13ad2f13ad9a7d513aaea01e0c807c76fa
alchemistbg <alchemist_bg@abv.bg>
2023-06-01 20:37:52 +0200

    Comment __init__ method for some of the classes because they are unnecessary

--- z-Python-03-OOP/10_-_E_-_Encapsulation/04_Restaurant/project/food/soup.py
+++ z-Python-03-OOP/10_-_E_-_Encapsulation/04_Restaurant/project/food/soup.py
@@ -3,5 +3,6 @@ from project.food.starter import Starter
 
 class Soup(Starter):
 
-    def __init__(self, name: str, price: float, grams: float):
-        super().__init__(name, price, grams)
+    # def __init__(self, name: str, price: float, grams: float):
+    #     super().__init__(name, price, grams)
+    pass
