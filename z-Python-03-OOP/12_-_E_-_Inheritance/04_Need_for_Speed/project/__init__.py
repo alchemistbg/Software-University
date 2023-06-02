549bc03d6c50e5c1f51c9a6d50317ad61cb762e2
alchemistbg <alchemist_bg@abv.bg>
2023-06-02 18:00:12 +0200

    Adding package exports in the __init__.py file

---
+++ z-Python-03-OOP/12_-_E_-_Inheritance/04_Need_for_Speed/project/__init__.py
@@ -0,0 +1,17 @@
+from project.vehicle import Vehicle
+from project.car import Car
+from project.family_car import FamilyCar
+from project.sport_car import SportCar
+from project.motorcycle import Motorcycle
+from project.race_motorcycle import RaceMotorcycle
+from project.cross_motorcycle import CrossMotorcycle
+
+__all__ = [
+	'Vehicle',
+	'Car',
+	'FamilyCar',
+	'SportCar',
+	'Motorcycle',
+	'RaceMotorcycle',
+	'CrossMotorcycle',
+]
