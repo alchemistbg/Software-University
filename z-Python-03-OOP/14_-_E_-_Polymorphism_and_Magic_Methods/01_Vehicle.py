837e7beb2b851d826e275da0f0e541ba54240e47
alchemistbg <alchemist_bg@abv.bg>
2023-06-06 18:10:00 +0200

    Add some testting code

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/01_Vehicle.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/01_Vehicle.py
@@ -9,3 +9,16 @@ class Vehicle(ABC):
 	@abstractmethod
 	def refuel(self):
 		self
+
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
+
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
