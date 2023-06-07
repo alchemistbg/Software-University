30af2529d88201b8c93324006ef8bd76ba016ad7
alchemistbg <alchemist_bg@abv.bg>
2023-06-07 20:14:40 +0200

    Add blank version of the 'Car' and 'Truck' classes

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/01_Vehicle.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/01_Vehicle.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class Vehicle(ABC):
 
 	@abstractmethod
@@ -11,14 +12,22 @@ class Vehicle(ABC):
 		self
 
 
+class Car(Vehicle):
+	pass
+
+
+class Truck(Vehicle):
+	pass
+
+
 car = Car(20, 5)
 car.drive(3)
 print(car.fuel_quantity)
 car.refuel(10)
 print(car.fuel_quantity)
 
-truck = Truck(100, 15)
-truck.drive(5)
-print(truck.fuel_quantity)
-truck.refuel(50)
-print(truck.fuel_quantity)
+# truck = Truck(100, 15)
+# truck.drive(5)
+# print(truck.fuel_quantity)
+# truck.refuel(50)
+# print(truck.fuel_quantity)
