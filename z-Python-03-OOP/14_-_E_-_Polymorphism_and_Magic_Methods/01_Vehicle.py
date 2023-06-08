8021396e27a72509652751d44c434a95e20c4344
alchemistbg <alchemist_bg@abv.bg>
2023-06-08 19:57:16 +0200

    Move the __init__ int abstract class

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/01_Vehicle.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/01_Vehicle.py
@@ -3,6 +3,10 @@ from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
 
+	def __init__(self, fuel_quantity, fuel_consumption):
+		self.fuel_quantity = fuel_quantity
+		self.fuel_consumption = fuel_consumption
+
 	@abstractmethod
 	def drive(self):
 		pass
@@ -13,9 +17,6 @@ class Vehicle(ABC):
 
 
 class Car(Vehicle):
-	def __init__(self, fuel_quantity, fuel_consumption ):
-		self.fuel_quantity = fuel_quantity
-		self.fuel_consumption = fuel_consumption
 
 	def drive(self):
 		pass
