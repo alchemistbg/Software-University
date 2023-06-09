200bfa7801b1fe1a9536b7f14559cb56c61a2504
alchemistbg <alchemist_bg@abv.bg>
2023-06-09 21:01:35 +0200

    Add some simple tests for the Person class

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/02_Groups.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/02_Groups.py
@@ -9,3 +9,13 @@ class Person:
 
 	def __repr__(self):
 		return f"{self.name} {self.surname}"
+
+
+p0 = Person('Aliko', 'Dangote')
+p1 = Person('Bill', 'Gates')
+p2 = Person('Warren', 'Buffet')
+p3 = Person('Elon', 'Musk')
+p4 = p2 + p3
+
+print(p0)
+print(p4)
