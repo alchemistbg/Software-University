7d31f8e3d379578ce404142e2e4b1a60ea1e1553
alchemistbg <alchemist_bg@abv.bg>
2023-06-13 16:08:33 +0200

    Reject ready to use tests from the problem's description Create custom-made tests based on the lecture's tests

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/01_Vehicle.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/01_Vehicle.py
@@ -45,14 +45,44 @@ class Truck(Vehicle):
 		self.fuel_quantity += (fuel * 0.95)
 
 
-car = Car(20, 5)
-car.drive(3)
-print(car.fuel_quantity)
-car.refuel(10)
-print(car.fuel_quantity)
+# Problem's author's test
+# car = Car(20, 5)
+# car.drive(3)
+# print(car.fuel_quantity)
+# car.refuel(10)
+# print(car.fuel_quantity)
+#
+# truck = Truck(100, 15)
+# truck.drive(5)
+# print(truck.fuel_quantity)
+# truck.refuel(50)
+# print(truck.fuel_quantity)
 
-truck = Truck(100, 15)
-truck.drive(5)
-print(truck.fuel_quantity)
-truck.refuel(50)
-print(truck.fuel_quantity)
+
+# Lecturer's custom tests
+def test_vehicle_can_be_instantiated():
+	print("Testing vehicle instantiation:")
+	print(42 * '-')
+	Car(20, 50)
+	print('Car instantiate Vehicle successfully')
+	Truck(20, 50)
+	print('Truck instantiate Vehicle successfully')
+	print(42 * '*')
+
+
+def test_vehicle_could_drive():
+	print("Testing vehicle driving:")
+	print(42 * '-')
+	c = Car(100, 5)
+	c.drive(2)
+	assert c.fuel_quantity == 88.2, c.fuel_quantity
+	print('Car can drive.')
+	t = Truck(100, 5)
+	t.drive(2)
+	assert t.fuel_quantity == 86.8, t.fuel_quantity
+	print('Truck can drive.')
+	print(42 * '*')
+
+
+test_vehicle_can_be_instantiated()
+test_vehicle_could_drive()
