a88809b05633313454edefa4f0e92cd8a3b1277c
alchemistbg <alchemist_bg@abv.bg>
2023-06-17 18:39:45 +0200

    Add some testing code

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/03_Account.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/03_Account.py
@@ -68,3 +68,76 @@ class Account:
 		account = Account(new_name, 10)
 		account._transactions = new_transactions[:]
 		return account
+
+# acc = Account('bob', 10)
+# acc2 = Account('john')
+# print(acc)
+# print(repr(acc))
+# acc.add_transaction(20)
+# acc.add_transaction(-20)
+# acc.add_transaction(30)
+# print(acc.balance)
+# print(len(acc))
+# for transaction in acc:
+# 	print(transaction)
+# print(acc[1])
+# print(list(reversed(acc)))
+# acc2.add_transaction(10)
+# acc2.add_transaction(60)
+# print(acc > acc2)
+# print(acc >= acc2)
+# print(acc < acc2)
+# print(acc <= acc2)
+# print(acc == acc2)
+# print(acc != acc2)
+# acc3 = acc + acc2
+# print(acc3)
+# print(acc3._transactions)
+# print(Account.validate_transaction(acc3, -111))
+
+
+# import unittest
+#
+#
+# class TestsAccount(unittest.TestCase):
+# 	def setUp(self):
+# 		self.acc1 = Account("Johhny")
+# 		self.acc2 = Account("Anna", 40)
+#
+# 	def test_init(self):
+# 		self.assertEqual(self.acc1.owner, "Johhny")
+# 		self.assertEqual(self.acc1.amount, 0)
+# 		self.assertEqual(self.acc1._transactions, [])
+# 		self.assertEqual(self.acc2.owner, "Anna")
+# 		self.assertEqual(self.acc2.amount, 40)
+# 		self.assertEqual(self.acc2._transactions, [])
+#
+# 	def test_repr(self):
+# 		self.assertEqual(repr(self.acc1), "Account(Johhny, 0)")
+#
+# 	def test_str(self):
+# 		self.assertEqual(str(self.acc2), "Account of Anna with starting amount: 40")
+#
+# 	def test_add_transaction(self):
+# 		self.acc1.add_transaction(10)
+# 		self.assertEqual(self.acc1._transactions, [10])
+# 		with self.assertRaises(ValueError) as cm:
+# 			self.acc1.add_transaction(9.9)
+# 		self.assertEqual(str(cm.exception), "please use int for amount")
+#
+# 	def test_balance(self):
+# 		self.acc2.add_transaction(-20)
+# 		self.assertEqual(self.acc2.balance, 20)
+#
+# 	def test_len(self):
+# 		self.acc1.add_transaction(10)
+# 		self.acc1.add_transaction(-5)
+# 		self.assertEqual(len(self.acc1), 2)
+#
+# 	def test_get_item(self):
+# 		self.acc1.add_transaction(5)
+# 		self.assertEqual(self.acc1[0], 5)
+#
+#
+# if __name__ == "__main__":
+# 	unittest.main()
