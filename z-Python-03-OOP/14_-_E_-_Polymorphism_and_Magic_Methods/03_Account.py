76b8175893a6e43ca68d77ee1449b2653088b0ea
alchemistbg <alchemist_bg@abv.bg>
2023-06-17 18:37:49 +0200

    Implement several magic methods They are needed to achieve the desired functionality

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/03_Account.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/03_Account.py
@@ -24,3 +24,40 @@ class Account:
 			raise ValueError("sorry cannot go in debt!")
 		account.add_transaction(amount_to_add)
 		return account.balance
+
+	def __str__(self):
+		return f"Account of {self.owner} with starting amount: {self.amount}"
+
+	def __repr__(self):
+		return f"Account({self.owner}, {self.amount})"
+
+	def __len__(self):
+		return len(self._transactions)
+
+	def __getitem__(self, key):
+		return self._transactions[key]
+
+	def __eq__(self, other: 'Account'):
+		return self.balance == other.balance
+
+	def __ne__(self, other: 'Account'):
+		return self.balance != other.balance
+
+	def __gt__(self, other: 'Account'):
+		return self.balance > other.balance
+
+	def __ge__(self, other: 'Account'):
+		return self.balance >= other.balance
+
+	def __lt__(self, other: 'Account'):
+		return self.balance < other.balance
+
+	def __le__(self, other: 'Account'):
+		return self.balance <= other.balance
+
+	def __add__(self, other: 'Account'):
+		new_name = f'{self.owner}&{other.owner}'
+		new_transactions = self._transactions + other._transactions
+		account = Account(new_name, 10)
+		account._transactions = new_transactions[:]
+		return account
