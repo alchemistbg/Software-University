70ddbb03d503556b1bcc4356e593cb10c1934d16
alchemistbg <alchemist_bg@abv.bg>
2023-06-17 18:25:02 +0200

    Implement add_transactions method

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/03_Account.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/03_Account.py
@@ -7,3 +7,9 @@ class Account:
 		self.owner = owner
 		self.amount = amount
 		self._transactions: List[int] = []
+
+	def add_transaction(self, amount: int):
+		if not isinstance(amount, int):
+			raise ValueError("please use int for amount")
+		self._transactions.append(amount)
+		
