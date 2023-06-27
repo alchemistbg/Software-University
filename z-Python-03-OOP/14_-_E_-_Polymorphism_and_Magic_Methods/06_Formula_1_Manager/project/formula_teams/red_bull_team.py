ca88d54f6d01e1742c7815d1287e07a546354ec1
alchemistbg <alchemist_bg@abv.bg>
2023-06-27 16:49:16 +0200

    Rewrite the class completely
    The class contains only the constants which are marked as abstract properties in the base class

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/06_Formula_1_Manager/project/formula_teams/red_bull_team.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/06_Formula_1_Manager/project/formula_teams/red_bull_team.py
@@ -3,19 +3,10 @@ from project.formula_teams.formula_team import FormulaTeam
 
 class RedBullTeam(FormulaTeam):
 
-    def __init__(self, budget: int):
-        super().__init__(budget)
-
-    def calculate_revenue_after_race(self, race_pos: int):
-        revenue = 0
-        if race_pos == 1:
-            revenue = 1520000
-        elif race_pos == 2:
-            revenue = 820000
-        elif race_pos == 8:
-            revenue = 20000
-        elif race_pos == 10:
-            revenue = 10000
-        revenue -= 250000
-        self.budget += revenue
-        return f"The revenue after the race is {revenue}$. Current budget {self.budget}$"
+    _RACE_COST = 250_000
+    _SPONSORS_PRICES = {
+        1: 1_520_000,
+        2: 820_000,
+        8: 20_000,
+        10: 10_000,
+    }
