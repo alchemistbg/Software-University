4635d774752cfb86fa6d4dc0b9ba6967ddd609e0
alchemistbg <alchemist_bg@abv.bg>
2023-06-27 16:47:17 +0200

    Rewrite some part of the class
    Define two new abstract properties which will hold inheritors' constants
    
    Move implementation of the calculate_revenue_after_race method from inheritors to the base class

--- z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/06_Formula_1_Manager/project/formula_teams/formula_team.py
+++ z-Python-03-OOP/14_-_E_-_Polymorphism_and_Magic_Methods/06_Formula_1_Manager/project/formula_teams/formula_team.py
@@ -7,17 +7,30 @@ class FormulaTeam(ABC):
         self.budget = budget
 
     @property
-    # @abstractmethod
     def budget(self):
         return self.__budget
 
     @budget.setter
-    # @abstractmethod
     def budget(self, value):
-        if value < 1000000:
+        if value < 1_000_000:
             raise ValueError('F1 is an expensive sport, find more sponsors!')
         self.__budget = value
 
+    @property
     @abstractmethod
-    def calculate_revenue_after_race(self, race_pos: int):
+    def _RACE_COST(self):
         ...
+
+    @property
+    @abstractmethod
+    def _SPONSORS_PRICES(self):
+        ...
+
+    def calculate_revenue_after_race(self, race_pos: int):
+        profit = 0
+        if race_pos in self._SPONSORS_PRICES.keys():
+            profit = self._SPONSORS_PRICES[race_pos]
+
+        revenue = profit - self._RACE_COST
+        self.budget += revenue
+        return f"The revenue after the race is {revenue}$. Current budget {self.budget}$"
