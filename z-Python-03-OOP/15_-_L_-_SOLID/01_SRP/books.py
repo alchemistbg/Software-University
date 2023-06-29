09df990342a6bc87cb1e6c5546b3130d1f34eec7
alchemistbg <alchemist_bg@abv.bg>
2023-06-29 20:17:44 +0200

    Refactor the code to satisfy the first principle 'Single responsibility'

--- z-Python-03-OOP/15_-_L_-_SOLID/01_SRP/books.py
+++ z-Python-03-OOP/15_-_L_-_SOLID/01_SRP/books.py
@@ -1,9 +1,24 @@
+from typing import List
+
 class Book:
-    def __init__(self, title, author, location):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.location = location
         self.page = 0
 
     def turn_page(self, page):
         self.page = page
+
+    def __str__(self):
+        return f"Book {self.name} by {self.author}"
+
+
+class Library:
+
+    def __init__(self, books: List[Book]):
+        self.books = books
+
+    def find_book(self, book_title):
+        for book in self.books:
+            if book.title == book_title:
+                return book 
