1809e21692815e4b996f6a334acba0413b9ee744
alchemistbg <alchemist_bg@abv.bg>
2023-06-29 20:31:22 +0200

    Perform additional code refatoring
    It is done mostly to add some real-life logic

--- z-Python-03-OOP/15_-_L_-_SOLID/01_SRP/books.py
+++ z-Python-03-OOP/15_-_L_-_SOLID/01_SRP/books.py
@@ -1,5 +1,10 @@
 from typing import List
 
+
+class BookNotFoundException(Exception):
+    pass
+
+
 class Book:
     def __init__(self, title, author):
         self.title = title
@@ -10,7 +15,7 @@ class Book:
         self.page = page
 
     def __str__(self):
-        return f"Book {self.name} by {self.author}"
+        return f"Book {self.title} by {self.author}"
 
 
 class Library:
@@ -19,6 +24,20 @@ class Library:
         self.books = books
 
     def find_book(self, book_title):
-        for book in self.books:
-            if book.title == book_title:
-                return book 
+        try:
+            book_result = [b for b in self.books if b.title == book_title]
+            if not book_result:
+                raise BookNotFoundException
+            return book_result[0]
+        except BookNotFoundException:
+            return f"Book {book_title} was not found!"
+
+
+book1 = Book('Book1', 'author1')
+book2 = Book('Book2', 'author2')
+book3 = Book('Book3', 'author3')
+
+library = Library([book1, book2, book3])
+
+print(library.find_book('Book1'))
+print(library.find_book('Book4'))
