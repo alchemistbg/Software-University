dfd9dd746c1e43eedc1ed6da92762a0f89c4cf7d
alchemistbg <alchemist_bg@abv.bg>
2023-06-30 17:40:32 +0200

    Implement a little different approach
    It uses implementation of make_sound() in the base class and abstract properties which are implemented in the child classes

---
+++ z-Python-03-OOP/15_-_L_-_SOLID/02_OCP/animal_-_my_idea.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+
+    @property
+    @abstractmethod
+    def _SOUND(self):
+        pass
+
+    def make_sound(self):
+        return self._SOUND
+
+
+class Cat(Animal):
+    _SOUND = "meow"
+
+
+class Dog(Animal):
+    _SOUND = "woof-woof"
+
+
+class Chicken(Animal):
+    _SOUND = "chick-chick"
+
+
+def animal_sound(animals: list[Animal]):
+    for animal in animals:
+        print(animal.make_sound())
+
+
+animals = [Cat(), Dog(), Chicken()]
+animal_sound(animals)
+
+## добавете ново животно и рефакторирайте кода да работи без да се налага да се правят промени по него
+## при добавяне на нови животни
+# animals = [Animal('cat'), Animal('dog'), Animal('chicken')]
