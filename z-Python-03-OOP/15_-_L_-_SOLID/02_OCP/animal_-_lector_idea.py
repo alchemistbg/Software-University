48fa400b8438f8cc9b1d43b2f27275a92282562a
alchemistbg <alchemist_bg@abv.bg>
2023-06-30 17:34:44 +0200

    Perform refactoring
    Delete commented code

--- z-Python-03-OOP/15_-_L_-_SOLID/02_OCP/animal_-_lector_idea.py
+++ z-Python-03-OOP/15_-_L_-_SOLID/02_OCP/animal_-_lector_idea.py
@@ -3,12 +3,6 @@ from abc import ABC, abstractmethod
 
 class Animal(ABC):
 
-    # def __init__(self, species):
-    #     self.species = species
-
-    # def get_species(self):
-    #     return self.species
-
     @abstractmethod
     def make_sound(self) -> str:
         pass
