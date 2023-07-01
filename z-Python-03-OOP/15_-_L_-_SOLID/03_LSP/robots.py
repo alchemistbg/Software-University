7322a9d1eae44a9891e81c4c876f76b34b5aa374
alchemistbg <alchemist_bg@abv.bg>
2023-07-01 20:35:15 +0200

    Refactor the code so it fulfills the Liskov substitution principle

--- z-Python-03-OOP/15_-_L_-_SOLID/03_LSP/robots.py
+++ z-Python-03-OOP/15_-_L_-_SOLID/03_LSP/robots.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+
+
 class Robot:
     def __init__(self, type):
         self.type = type
@@ -5,23 +8,32 @@ class Robot:
     def get_type(self):
         return self.type
 
+    @abstractmethod
+    def get_sensors_count(self):
+        pass
+
 
 class Android(Robot):
-    def android_senzors_count(self):
+    # def android_senzors_count(self):
+    #     return 4
+    def get_sensors_count(self):
         return 4
 
 
 class Chappie(Robot):
-    def chappie_senzors_count(self):
+    # def chappie_senzors_count(self):
+    #     return 6
+    def get_sensors_count(self):
         return 6
 
 
-def count_robot_senzors(robots: list):
+def count_robot_senzors(robots: list[Robot]):
     for robot in robots:
-        if isinstance(robot, Android):
-            print(robot.android_senzors_count())
-        elif isinstance(robot, Chappie):
-            print(robot.chappie_senzors_count())
+        # if isinstance(robot, Android):
+        #     print(robot.android_senzors_count())
+        # elif isinstance(robot, Chappie):
+        #     print(robot.chappie_senzors_count())
+        print(robot.get_sensors_count())
 
 
 robots = [Android('Robocop'), Chappie('XIX')]
