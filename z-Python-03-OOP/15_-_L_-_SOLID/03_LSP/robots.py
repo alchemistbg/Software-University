782edcab2580f1747c76ce6d8537c14cd67f0a67
alchemistbg <alchemist_bg@abv.bg>
2023-07-02 12:36:52 +0200

    Clean the code from comments and junk

--- z-Python-03-OOP/15_-_L_-_SOLID/03_LSP/robots.py
+++ z-Python-03-OOP/15_-_L_-_SOLID/03_LSP/robots.py
@@ -1,12 +1,7 @@
 from abc import ABC, abstractmethod
 
 
-class Robot:
-    def __init__(self, type):
-        self.type = type
-
-    def get_type(self):
-        return self.type
+class Robot(ABC):
 
     @abstractmethod
     def get_sensors_count(self):
@@ -14,27 +9,19 @@ class Robot:
 
 
 class Android(Robot):
-    # def android_senzors_count(self):
-    #     return 4
     def get_sensors_count(self):
         return 4
 
 
 class Chappie(Robot):
-    # def chappie_senzors_count(self):
-    #     return 6
     def get_sensors_count(self):
         return 6
 
 
 def count_robot_senzors(robots: list[Robot]):
     for robot in robots:
-        # if isinstance(robot, Android):
-        #     print(robot.android_senzors_count())
-        # elif isinstance(robot, Chappie):
-        #     print(robot.chappie_senzors_count())
-        print(robot.get_sensors_count())
+        print(f"{robot.__class__.__name__} has {robot.get_sensors_count()} sensors")
 
 
-robots = [Android('Robocop'), Chappie('XIX')]
+robots = [Android(), Chappie()]
 count_robot_senzors(robots)
