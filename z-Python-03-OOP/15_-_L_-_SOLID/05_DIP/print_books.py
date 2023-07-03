1653e86b5e5f3a617ab960a84d46963bf228a44a
alchemistbg <alchemist_bg@abv.bg>
2023-07-03 20:20:55 +0200

    Refactor the code so that it fulfills dependency inversion principle

--- z-Python-03-OOP/15_-_L_-_SOLID/05_DIP/print_books.py
+++ z-Python-03-OOP/15_-_L_-_SOLID/05_DIP/print_books.py
@@ -1,15 +1,40 @@
+from abc import ABC, abstractmethod
+
+
 class Book:
-    def __init__(self, content: str):
+
+    def __init__(self, title, author, content):
+        self.title = title
+        self.author = author
         self.content = content
 
 
-class Formatter:
-    def format(self, book: Book) -> str:
+class Formatter(ABC):
+
+    @abstractmethod
+    def format(self, book: Book):
+        pass
+
+
+class ContentFormatter(Formatter):
+
+    def format(self, book: Book):
         return book.content
 
 
+class CoverFormatter(Formatter):
+
+    def format(self, book: Book):
+        return f'{book.title}\nby\n{book.author}'
+
+
 class Printer:
-    def get_book(self, book: Book):
-        formatter = Formatter()
-        formatted_book = formatter.format(book)
-        return formatted_book
+
+    def print(self, book: Book, formatter: Formatter):
+        return formatter.format(book)
+
+
+book = Book('Nice book', 'Nice Author', 'A very nice content')
+printer = Printer()
+print(printer.print(book, CoverFormatter()))
+print(printer.print(book, ContentFormatter()))
