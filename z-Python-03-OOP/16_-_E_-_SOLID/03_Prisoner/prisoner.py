170a2455fa47582c689d2a9bd1d9aaa97ade0120
alchemistbg <alchemist_bg@abv.bg>
2023-07-07 20:18:13 +0200

    Add solution to the 3rd problem from the exercise for the lecture 'SOLID'

--- z-Python-03-OOP/16_-_E_-_SOLID/03_Prisoner/prisoner.py
+++ z-Python-03-OOP/16_-_E_-_SOLID/03_Prisoner/prisoner.py
@@ -1,9 +1,16 @@
 import copy
 
+
 class Person:
 
     def __init__(self, position):
         self.position = position
+
+
+class FreePerson(Person):
+
+    def __init__(self, position):
+        super().__init__(position)
 
     def walk_north(self, dist):
         self.position[1] += dist
@@ -11,14 +18,13 @@ class Person:
     def walk_east(self, dist):
         self.position[0] += dist
 
-
+    
 class Prisoner(Person):
     PRISON_LOCATION = [3, 3]
 
     def __init__(self):
         super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
         self.is_free = False
-
 
 
 prisoner = Prisoner()
