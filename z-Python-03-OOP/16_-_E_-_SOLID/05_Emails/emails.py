8cad897da218bb6f16d70760ec6708b569ecae3f
alchemistbg <alchemist_bg@abv.bg>
2023-07-08 19:29:50 +0200

    Add solution to the 5th problem from the exercise for the lecture 'SOLID'

--- z-Python-03-OOP/16_-_E_-_SOLID/05_Emails/emails.py
+++ z-Python-03-OOP/16_-_E_-_SOLID/05_Emails/emails.py
@@ -1,7 +1,7 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
-class IEmail(object):
-    __metaclass__ = ABCMeta
+
+class IEmail(ABC):
 
     @abstractmethod
     def set_sender(self, sender):
@@ -15,11 +15,11 @@ class IEmail(object):
     def set_content(self, content):
         pass
 
+
 class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
@@ -37,21 +37,38 @@ class Email(IEmail):
             self.__receiver = receiver
 
     def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+        self.__content = content.format()
 
     def __repr__(self):
-
         template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
-
         return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
 
 
-email = Email('IM', 'MyML')
+class IContent(ABC):
+
+    def __init__(self, text):
+        self.text = text
+
+    @abstractmethod
+    def format(self):
+        pass
+
+
+class MyContent(IContent):
+
+    def format(self):
+        return ''.join(['<MyML>', self.text, '</MyML>'])
+
+
+email = Email('IM')
 email.set_sender('qmal')
 email.set_receiver('james')
 email.set_content('Hello, there!')
 print(email)
 
+email = Email('IM')
+email.set_sender('qmal')
+email.set_receiver('james')
+content = MyContent('Hello, there!')
+email.set_content(content)
+print(email)
