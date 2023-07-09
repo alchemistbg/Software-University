510a342c3f5599e512849f04328ccd9395971c30
alchemistbg <alchemist_bg@abv.bg>
2023-07-09 20:40:49 +0200

    Refactor the code in the main.py file in order to be compliant with SOLID

--- z-Python-03-OOP/z_-_01_-_Small_Project/main.py
+++ z-Python-03-OOP/z_-_01_-_Small_Project/main.py
@@ -1,13 +1,36 @@
 from datetime import timedelta
 from report_generator import generate_report
-from report_formatter import format_report
+from report_formatter import plain_text_format, line_chart_format
 from email_sender import send_email
 
 
+class CpuReporter:
+
+	def __init__(self, generator, formatter, sender):
+		self.__generator = generator
+		self.__reporter = formatter
+		self.__sender = sender
+
+	def send_report(self):
+		report = self.__generator(timedelta(seconds = 10))
+		formatted = self.__reporter(report)
+		self.__sender(formatted)
+
+
 def main():
-	report = generate_report(timedelta(seconds = 1))
-	formatted = format_report(report)
-	send_email(formatted)
+	plain_text_reporter = CpuReporter(
+		generator=generate_report,
+		formatter=plain_text_format,
+		sender=send_email
+	)
+
+	fancy_reporter = CpuReporter(
+		generator=generate_report,
+		formatter=line_chart_format,
+		sender=send_email
+	)
+
+	fancy_reporter.send_report()
 
 
 if __name__ == '__main__':
