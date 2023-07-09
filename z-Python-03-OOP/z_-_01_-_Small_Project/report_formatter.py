f7779de9cf543c365ef0e5346cc62ad027ea506f
alchemistbg <alchemist_bg@abv.bg>
2023-07-09 20:41:17 +0200

    Refactor the code in the report_formatter.py file in order to be compliant with SOLID

--- z-Python-03-OOP/z_-_01_-_Small_Project/report_formatter.py
+++ z-Python-03-OOP/z_-_01_-_Small_Project/report_formatter.py
@@ -1,9 +1,40 @@
-from datetime import datetime
+import sys
+import io
 from typing import List, Tuple
+from datetime import datetime
+import matplotlib.pyplot as plt
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
 
-def format_report(report: List[Tuple[datetime, float]]):
+def plain_text_format(report: List[Tuple[datetime, List[float]]]):
 	return '\n'.join([
 		f"{time.strftime('%H:%M:%S.%f')} {measurement}"
 		for time, measurement in report
 	])
+
+
+def line_chart_format(report: List[Tuple[datetime, float]]):
+	time = [time for time, _ in report]
+	measurements = [measurement for _, measurement in report]
+
+	plt.plot(time, measurements, color = 'green')
+	plt.xlabel('Time')
+	plt.xticks(rotation = 45)
+	plt.ylabel('CPU Load (%)')
+	plt.title('CPU Report')
+
+	pdf_file = io.BytesIO()
+	plt.savefig(pdf_file, bbox_inches = 'tight', format = 'pdf')
+
+	email = MIMEMultipart()
+	part = MIMEBase('application', "octet-stream")
+	part.set_payload(pdf_file.getvalue())
+	encoders.encode_base64(part)
+	part.add_header(
+		"Content-Disposition",
+		"attachment; filename = report.pdf",
+	)
+	email.attach(part)
+	return email
