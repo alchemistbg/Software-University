622c3f097e7f9cca78fa5277ee3ad83422f5edc7
alchemistbg <alchemist_bg@abv.bg>
2023-07-21 22:24:56 +0200

    Add some democode from the lab for the 'Testing'

---
+++ z-Python-03-OOP/21_-_L_-_Testing/demo/_demo.py
@@ -0,0 +1,12 @@
+# import unittest
+#
+#
+# class SimpleTest(unittest.TestCase):
+# 	def test_upper(self):
+# 		result = 'foo'.upper()
+# 		expected = 'FOO'
+# 		self.assertEquals(result, expected)
+#
+#
+# if __name__ == 'main':
+# 	unittest.main()
