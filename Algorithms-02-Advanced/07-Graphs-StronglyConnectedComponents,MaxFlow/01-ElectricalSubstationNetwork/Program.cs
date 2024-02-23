2fcbcd125a454d927232bd281805ee2d693e05e4
alchemistbg <alchemist_bg@abv.bg>
2024-02-23 19:06:04 +0100

    Add files for the exercise 'Strongly Connected Components, MaxFLow'

---
+++ Algorithms-02-Advanced/07-Graphs-StronglyConnectedComponents,MaxFlow/01-ElectricalSubstationNetwork/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_ElectricalSubstationNetwork
+{
+    class Program
+    {
+        public static List<int>[] originalGraph;
+        public static List<int>[] reversedGraph;
+        public static Stack<int> orderedNodes;
+
+        static void Main(string[] args)
+        {
+            int nodesCount = int.Parse(Console.ReadLine());
+            int edgesCount = int.Parse(Console.ReadLine());
+
+            (originalGraph, reversedGraph) = ReadGraph(nodesCount, edgesCount);
+
+            orderedNodes = TopologicalSorting();
+
+            bool[] visitedReversed = new bool[nodesCount]; 
+            while (orderedNodes.Count > 0)
+            {
+                int currentNode = orderedNodes.Pop();
+
+                if (visitedReversed[currentNode])
+                {
+                    continue;
+                }
+
+                Stack<int> scc = new Stack<int>();
+                DFS(reversedGraph, currentNode, visitedReversed, scc);
+                Console.WriteLine(string.Join(", ", scc));
+            }
+        }
+
+        private static Stack<int> TopologicalSorting()
+        {
+            Stack<int> stack = new Stack<int>();
+            bool[] visitedOriginal = new bool[originalGraph.Length];
+
+            for (int node = 0; node < originalGraph.Length; node++)
+            {
+                if (!visitedOriginal[node])
+                {
+                    DFS(originalGraph, node, visitedOriginal, stack);
+                }
+            }
+
+            return stack;
+        }
+
+        private static void DFS(List<int>[] workingGraph, int node, bool[] visited, Stack<int> stack)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (int child in workingGraph[node])
+            {
+                DFS(workingGraph, child, visited, stack);
+            }
+
+            stack.Push(node);
+        }
+
+        private static (List<int>[] original, List<int>[] reversed) ReadGraph(int nodesCount, int edgesCount)
+        {
+            List<int>[] original = new List<int>[nodesCount];
+            List<int>[] reversed = new List<int>[nodesCount];
+
+            for (int node = 0; node < nodesCount; node++)
+            {
+                original[node] = new List<int>();
+                reversed[node] = new List<int>();
+            }
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                int[] data = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                int node = data[0];
+                for (int j = 1; j < data.Length; j++)
+                {
+                    int child = data[j];
+                    original[node].Add(child);
+                    reversed[child].Add(node);
+                }
+            }
+
+            return (original, reversed);
+        }
+    }
+}
---
+++ Algorithms-02-Advanced/07-Graphs-StronglyConnectedComponents,MaxFlow/02-MaximumTasksAssignment/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_MaximumTasksAssignment
+{
+    class Program
+    {
+        private static int[,] graph;
+        private static int[] parents;
+        static void Main(string[] args)
+        {
+            int workersCount = int.Parse(Console.ReadLine());
+            int tasksCount = int.Parse(Console.ReadLine());
+
+            int nodesCount = 2 + workersCount + tasksCount;
+
+            int start = 0;
+            int target = nodesCount - 1;
+
+            graph = InitGraph(workersCount, tasksCount, nodesCount);
+            parents = new int[graph.GetLength(0)];
+            for (int parent = 0; parent < parents.Length; parent++)
+            {
+                parents[parent] = -1;
+            }
+
+            FillGraph(workersCount);
+
+            PerformMaxFlow(workersCount, tasksCount, start, target);
+
+            //PrintGraph();
+        }
+
+        private static void PerformMaxFlow(int workers, int tasks, int start, int target)
+        {
+            while (BFS(start, target))
+            {
+                int currentNode = target;
+
+                while (currentNode != start)
+                {
+                    int parent = parents[currentNode];
+
+                    graph[parent, currentNode] = 0;
+                    graph[currentNode, parent] = 1;
+
+                    currentNode = parent;
+                }
+            }
+
+            for (int worker = 1; worker <= workers; worker++)
+            {
+                for (int task = workers + 1; task <= workers + tasks; task++)
+                { 
+                    if (graph[task, worker] > 0)
+                    {
+                        Console.WriteLine($"{(char)(64 + worker)}-{task - workers}");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static bool BFS(int start, int target)
+        {
+            bool[] visited = new bool[graph.GetLength(0)];
+
+            Queue<int> queue = new Queue<int>();
+
+            visited[start] = true;
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                int currentNode = queue.Dequeue();
+
+                if (currentNode == target)
+                {
+                    return true;
+                }
+
+                for (int child = 0; child < graph.GetLength(1); child++)
+                {
+                    if (!visited[child] && graph[currentNode, child] > 0)
+                    {
+                        parents[child] = currentNode;
+                        visited[child] = true;
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static void FillGraph(int workersCount)
+        {
+            for (int i = 1; i <= workersCount; i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[j] == 'Y')
+                    {
+                        graph[i, workersCount + 1 + j] = 1;
+                    }
+                }
+            }
+        }
+
+        private static int[,] InitGraph(int workersCount, int tasksCount, int nodesCount)
+        {
+            int[,] result = new int[nodesCount, nodesCount];
+
+            for (int worker = 1; worker <= workersCount; worker++)
+            {
+                result[0, worker] = 1;
+            }
+
+            for (int task = workersCount + 1; task <= workersCount + tasksCount; task++)
+            {
+                result[task, nodesCount - 1] = 1;
+            }
+
+            return result;
+        }
+
+        public static void PrintGraph()
+        {
+            for (int r = 0; r < graph.GetLength(0); r++)
+            {
+                for (int c = 0; c < graph.GetLength(1); c++)
+                {
+                    Console.Write($"{graph[r, c]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
