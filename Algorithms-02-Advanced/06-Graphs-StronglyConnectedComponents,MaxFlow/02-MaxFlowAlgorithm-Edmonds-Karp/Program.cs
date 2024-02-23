381fa882a7c59d7ecc847bad998e265cd0d9e59e
alchemistbg <alchemist_bg@abv.bg>
2024-02-23 19:05:41 +0100

    Add files for the lecture 'Strongly Connected Components, MaxFLow'

---
+++ Algorithms-02-Advanced/06-Graphs-StronglyConnectedComponents,MaxFlow/02-MaxFlowAlgorithm-Edmonds-Karp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_MaxFlowAlgorithm_Edmonds_Karp
+{
+    class Program
+    {
+        public static int nodesCount, source, destination, maxFlow;
+        public static int[][] graph;
+        public static int[] parents;
+
+        static void Main(string[] args)
+        {
+            ReadInput();
+            FindMaxFlow();
+            PrintResults();
+        }
+
+        private static void FindMaxFlow()
+        {
+            maxFlow = 0;
+            while (BFS(graph, parents))
+            {
+                var minFlow = int.MaxValue;
+                var to = destination;
+                var from = parents[to];
+
+                while (to != -1 && from != -1)
+                {
+                    minFlow = Math.Min(minFlow, graph[from][to]);
+
+                    to = parents[to];
+                    from = parents[to];
+                }
+
+                maxFlow += minFlow;
+                to = destination;
+                from = parents[to];
+                while (to != -1 && from != -1)
+                {
+                    graph[from][to] -= minFlow;
+                    to = parents[to];
+                    from = parents[to];
+                }
+            }
+        }
+
+        private static bool BFS(int[][] graph, int[] parents)
+        {
+            var visited = new bool[graph.Length];
+            var queue = new Queue<int>();
+
+            visited[source] = true;
+            queue.Enqueue(source);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                for (int child = 0; child < graph[node].Length; child++)
+                {
+                    if (!visited[child] && graph[node][child] > 0)
+                    {
+                        queue.Enqueue(child);
+                        visited[child] = true;
+                        parents[child] = node;
+                    }
+                }
+            }
+
+            return visited[destination];
+        }
+
+        private static void PrintResults()
+        {
+            Console.WriteLine($"Max flow = " + maxFlow);
+        }
+
+        private static void ReadInput()
+        {
+            nodesCount = int.Parse(Console.ReadLine());
+            graph = new int[nodesCount][];
+            parents = new int[nodesCount];
+            for (int i = 0; i < nodesCount; i++)
+            {
+                var elements = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+                graph[i] = elements;
+            }
+
+            source = int.Parse(Console.ReadLine());
+            destination = int.Parse(Console.ReadLine());
+            Array.Fill(parents, -1);
+        }
+    }
+}
---
+++ Algorithms-02-Advanced/06-Graphs-StronglyConnectedComponents,MaxFlow/03-ArticulationPoints/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_ArticulationPoints
+{
+    class Program
+    {
+        public static int nodesCount;
+        public static List<int>[] graph;
+        public static int[] parents, depths, lowpoints;
+        public static bool[] visited;
+        public static List<int> points;
+        static void Main(string[] args)
+        {
+            ReadInput();
+            DFS(0, 1);
+            PrintResult();
+        }
+        private static void DFS(int node, int depth)
+        {
+            visited[node] = true;
+            depths[node] = depth;
+            lowpoints[node] = depth;
+
+            var children = 0;
+            var isArticulation = false;
+
+            foreach (var child in graph[node])
+            {
+                if (!visited[child])
+                {
+                    parents[child] = node;
+                    DFS(child, depth + 1);
+                    children++;
+
+                    if (lowpoints[child] >= depth)
+                    {
+                        isArticulation = true;
+                    }
+
+                    lowpoints[node] = Math.Min(lowpoints[node], lowpoints[child]);
+                }
+                else if (parents[node] != child)
+                {
+                    lowpoints[node] = Math.Min(lowpoints[node], depths[child]);
+                }
+            }
+
+            if (parents[node] == -1 && children > 1 || parents[node] != -1 && isArticulation)
+            {
+                points.Add(node);
+            }
+        }
+        private static void PrintResult()
+        {
+            Console.WriteLine($"Articulation points: {string.Join(", ", points)}");
+        }
+        private static void InitializeVariables()
+        {
+            depths = new int[nodesCount];
+            lowpoints = new int[nodesCount];
+            graph = new List<int>[nodesCount];
+            parents = new int[nodesCount];
+            Array.Fill(parents, -1);
+            visited = new bool[nodesCount];
+            points = new List<int>();
+        }
+        private static void ReadInput()
+        {
+            nodesCount = int.Parse(Console.ReadLine());
+            int lines = int.Parse(Console.ReadLine());
+            InitializeVariables();
+            for (int i = 0; i < lines; i++)
+            {
+                var elements = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToList();
+                graph[i] = elements.Skip(1).ToList();
+            }
+        }
+    }
+}
