390fe42b28cf04d1013fed6982d555385ba330cb
alchemistbg <alchemist_bg@abv.bg>
2024-03-02 19:16:20 +0100

    Add the code from the exercise 'Exam Preparation'

---
+++ DataStructures-01-Fundamentals/10-ExamPreparation/01.Loader/Loader.cs
@@ -0,0 +1,176 @@
+﻿namespace _01.Loader
+{
+    using _01.Loader.Interfaces;
+    using _01.Loader.Models;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Loader : IBuffer
+    {
+        public List<IEntity> _entities;
+        public Loader()
+        {
+            this._entities = new List<IEntity>();
+        }
+        public int EntitiesCount => this._entities.Count;
+
+        public void Add(IEntity entity)
+        {
+            //throw new NotImplementedException();
+            this._entities.Add(entity);
+        }
+
+        public void Clear()
+        {
+            //throw new NotImplementedException();
+            this._entities.Clear();
+        }
+
+        public bool Contains(IEntity entity)
+        {
+            //throw new NotImplementedException();
+            return this.GetById(entity.Id) != null;
+        }
+
+        public IEntity Extract(int id)
+        {
+            //throw new NotImplementedException();
+            IEntity result = this.GetById(id);
+
+            if (result != null)
+            {
+                this._entities.Remove(result);
+            }
+
+            return result;
+        }
+
+        public IEntity Find(IEntity entity)
+        {
+            //throw new NotImplementedException();
+            return this.GetById(entity.Id);
+        }
+
+        public List<IEntity> GetAll()
+        {
+            //throw new NotImplementedException();
+            return new List<IEntity>(this._entities);
+        }
+
+        public void RemoveSold()
+        {
+            //throw new NotImplementedException();
+            this._entities.RemoveAll(e => e.Status == BaseEntityStatus.Sold);
+        }
+
+        public void Replace(IEntity oldEntity, IEntity newEntity)
+        {
+            //throw new NotImplementedException();
+            int oldEntityIndex = this.GetEntityIndex(oldEntity);
+
+            this.ValidateEntityByIndex(oldEntityIndex);
+
+            this._entities[oldEntityIndex] = newEntity;
+        }
+
+        public List<IEntity> RetainAllFromTo(BaseEntityStatus lowerBound, BaseEntityStatus upperBound)
+        {
+            //throw new NotImplementedException();
+            List<IEntity> result = new List<IEntity>(this.EntitiesCount);
+
+            int lowerBoundInt = (int)lowerBound;
+            int upperBoundInt = (int)upperBound;
+
+            for (int i = 0; i < this.EntitiesCount; i++)
+            {
+                IEntity current = this._entities[i];
+                int currentStatusInt = (int)current.Status;
+                if (currentStatusInt >= lowerBoundInt
+                    && currentStatusInt <= upperBoundInt)
+                {
+                    result.Add(current);
+                }
+            }
+
+            return result;
+        }
+
+        public void Swap(IEntity first, IEntity second)
+        {
+            //throw new NotImplementedException();
+            int firstEntityIndex = this.GetEntityIndex(first);
+            this.ValidateEntityByIndex(firstEntityIndex);
+
+            int secondEntityIndex = this.GetEntityIndex(second);
+            this.ValidateEntityByIndex(secondEntityIndex);
+
+            IEntity temp = this._entities[firstEntityIndex];
+            this._entities[firstEntityIndex] = this._entities[secondEntityIndex];
+            this._entities[secondEntityIndex] = temp;
+        }
+
+        public IEntity[] ToArray()
+        {
+            //throw new NotImplementedException();
+            return this._entities.ToArray();
+        }
+
+        public void UpdateAll(BaseEntityStatus oldStatus, BaseEntityStatus newStatus)
+        {
+            //throw new NotImplementedException();
+            for (int i = 0; i < this._entities.Count; i++)
+            {
+                IEntity current = this._entities[i];
+
+                if (current.Status == oldStatus)
+                {
+                    current.Status = newStatus;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //throw new NotImplementedException();
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<IEntity> GetEnumerator()
+        {
+            //throw new NotImplementedException();
+            for (int i = 0; i < this._entities.Count; i++)
+            {
+                yield return this._entities[i];
+            }
+        }
+
+        private IEntity GetById(int Id)
+        {
+            for (int i = 0; i < this._entities.Count; i++)
+            {
+                IEntity current = this._entities[i];
+                if (current.Id == Id)
+                {
+                    return current;
+                }
+            }
+
+            return null;
+        }
+
+        private int GetEntityIndex(IEntity entity)
+        {
+            return this._entities.IndexOf(entity);
+        }
+
+        private void ValidateEntityByIndex(int index)
+        {
+            //throw new NotImplementedException();
+            if (index < 0)
+            {
+                throw new InvalidOperationException("Error!");
+            }
+        }
+    }
+}
---
+++ DataStructures-01-Fundamentals/10-ExamPreparation/02.Data/Data.cs
@@ -0,0 +1,121 @@
+﻿namespace _02.Data
+{
+    using _02.Data.Interfaces;
+    using _02.Data.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using Wintellect.PowerCollections;
+
+    public class Data : IRepository
+    {
+
+        OrderedBag<IEntity> _entities;
+
+        public Data()
+        {
+            this._entities = new OrderedBag<IEntity>();
+        }
+        public Data(Data data)
+        {
+            this._entities = data._entities;
+        }
+
+        public int Size => this._entities.Count;
+
+        public void Add(IEntity entity)
+        {
+            //throw new NotImplementedException();
+            this._entities.Add(entity);
+
+            IEntity parentNode = this.GetById((int)entity.ParentId);
+            if (parentNode != null)
+            {
+                parentNode.Children.Add(entity);
+            }
+        }
+
+        public IRepository Copy()
+        {
+            //throw new NotImplementedException();
+            Data copy = (Data)this.MemberwiseClone();
+            return new Data(copy);
+        }
+
+        public List<IEntity> GetAll()
+        {
+            //throw new NotImplementedException();
+            return new List<IEntity>(this._entities);
+        }
+
+        public List<IEntity> GetAllByType(string type)
+        {
+            //throw new NotImplementedException();
+            if (type != typeof(Invoice).Name
+                && type != typeof(User).Name
+                && type != typeof(StoreClient).Name)
+            {
+                throw new InvalidOperationException($"Invalid type: {type}");
+            }
+
+            List<IEntity> result = new List<IEntity>(this.Size);
+
+            for (int i = 0; i < this.Size; i++)
+            {
+                IEntity current = this._entities[i];
+                if (current.GetType().Name == type)
+                {
+                    result.Add(current);
+                }
+            }
+
+            return result;
+        }
+
+        public IEntity GetById(int id)
+        {
+            //throw new NotImplementedException();
+            if (id < 0 || id >= this.Size)
+            {
+                return null;
+            }
+            return this._entities[this.Size - 1 - id];
+        }
+
+        public List<IEntity> GetByParentId(int parentId)
+        {
+            //throw new NotImplementedException();
+            List<IEntity> result = new List<IEntity>();
+
+            IEntity parentNode = this.GetById(parentId);
+            if (parentNode != null)
+            {
+                result = new List<IEntity>(parentNode.Children);
+            }
+
+            return result;
+        }
+
+        public IEntity PeekMostRecent()
+        {
+            //throw new NotImplementedException();
+            this.EnsureNotEmpty();
+            return this._entities.GetFirst();
+        }
+
+        public IEntity DequeueMostRecent()
+        {
+            //throw new NotImplementedException();
+            this.EnsureNotEmpty();
+            return this._entities.RemoveFirst();
+        }
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException("Operation on empty Data");
+            }
+        }
+    }
+}
